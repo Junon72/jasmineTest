c608a06055b49b86614ed8da7c23b8e4adef7510
Jussi Nousiainen <juno.athome@gmail.com>
2019-10-07 18:41:56 +0200

    corrected the calcspec.js lines

--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -1,9 +1,7 @@
-describe("Calculator", function () {
-    describe("Addition test", function () {
-        it("should return 42", function () {
-            expected(addition(20, 22)).toBe(42);
+describe("Calculator", function() {
+    describe("Addition function", function() {
+        it("should return 42", function() {
+            expect(addition(20, 22)).toBe(42);
         });
-
     });
-
 });
